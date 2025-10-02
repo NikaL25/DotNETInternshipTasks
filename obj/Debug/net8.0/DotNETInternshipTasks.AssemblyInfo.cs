08377d0f5fcd652a59ba5d6f7a41d52f9ad6aa55
NikaL25 <mr.lomiashvil@mail.ru>
2025-10-03 02:40:04 +0400

    added Readme file

--- obj/Debug/net8.0/DotNETInternshipTasks.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNETInternshipTasks.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNETInternshipTasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7947e23e452ba6eb76080373fbd6a30bdcfa8a35")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNETInternshipTasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNETInternshipTasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
